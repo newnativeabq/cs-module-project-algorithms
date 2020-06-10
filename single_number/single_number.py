3884c5465d0fd79c1c9ac007e98c5cd2cc7c9131
newnativeabq <vincent.a.brandon@gmail.com>
2020-06-09 21:26:21 -0600

    UPD: single number working with hash imp

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,26 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    counts = {}    
+    val = None
+    for num in arr:
+        if num in counts:
+            counts[num] += 1
+        else:
+            counts[num] = 1
+    return [key for key in counts if counts[key] == 1][0]
 
-    pass
+
+def single_number_xor_test(arr):
+    val = 0
+    for num in arr:
+        val ^= num 
+    return num
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
+    # print(f"The odd-number-out is {single_number_hash(arr)}")
     print(f"The odd-number-out is {single_number(arr)}")
