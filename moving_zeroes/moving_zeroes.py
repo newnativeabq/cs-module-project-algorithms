52d5271e40fcb2135ca5a1072f9ebc5de15dfba1
newnativeabq <vincent.a.brandon@gmail.com>
2020-06-11 21:21:25 -0600

    ADD: completed moving zeroes and sliding windows implementations

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,28 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+def _find_non_zero(subset):
+    for i, val in enumerate(subset):
+        if val != 0:
+            return i
+
+
+def _swap(x1, x2):
+    return x2, x1
+
+
+def moving_zeroes(arr):
+    for i, val in enumerate(arr):
+        if arr[i] == 0:
+            subset_index = _find_non_zero(arr[i:])
+            if subset_index is None:
+                break
+            swap_pos = i + subset_index 
+            arr[i], arr[swap_pos] = _swap(arr[i], arr[swap_pos])
+
+    return arr
+
 
 
 if __name__ == '__main__':
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,74 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
 
-    pass
+class Window():
+    def __init__(self, k, max_len):
+        self.k = k
+        self.max_len = max_len
+        self.min = 0
+        if k < max_len:
+            self.max = k
+        else:
+            self.max = max_len
+
+
+    def _return_valid_max(self):
+        if self.max <= self.max_len:
+            return self.max 
+        return self.max_len
+
+
+    def shift(self, m=1):
+        self.min += m 
+        self.max += m 
+        return self.min, self.max
+
+
+class Bounds():
+    def __init__(self, mini, maxi):
+        self.min = mini
+        self.max = maxi
+
+
+
+def sliding_window_max(arr, k):
+    window = Window(k=k, max_len=len(arr))
+    max_arr = []
+    for _ in arr:
+        max_arr.append(
+            max(arr[window.min:window.max])
+        )
+        if window.max == window.max_len:
+            break
+        window.shift()
+    return max_arr
+
+
+
+# def _get_max(job):
+#     arr = job[0]
+#     bounds = job[1]
+#     return max(arr[bounds.min:bounds.max])
+
+# def _build_bounds(arr, k):
+#     window = Window(k=k, max_len=len(arr))
+#     bounds= []
+#     for _ in arr:
+#         bounds.append(
+#             Bounds(mini=window.min, maxi=window.max)
+#         )
+#         if window.max == window.max_len:
+#             break
+#         window.shift()
+#     return bounds
+
+# def sliding_window_max(arr, k):
+#     bounds = _build_bounds(arr, k)
+#     return list(map(
+#         _get_max, list(zip([arr]*len(bounds), bounds))
+#     ))
+
 
 
 if __name__ == '__main__':
